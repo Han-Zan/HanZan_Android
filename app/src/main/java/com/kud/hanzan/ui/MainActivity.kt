4c813f539c462d6d2d593aa3f73cf366bef0b9dd
active_dg <nerw173@gmail.com>
2022-09-02 18:46:54 +0900

    [UPD] 메인화면 바텀내비 수정

--- app/src/main/java/com/kud/hanzan/ui/MainActivity.kt
+++ app/src/main/java/com/kud/hanzan/ui/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.kud.hanzan.ui
 
+import android.content.Intent
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.kud.hanzan.R
 import com.kud.hanzan.databinding.ActivityMainBinding
+import com.kud.hanzan.ui.search.SearchActivity
 import com.kud.hanzan.utils.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,5 +19,16 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main){
     private fun initBottomNav(){
         val navController = supportFragmentManager.findFragmentById(R.id.main_fragment_container)?.findNavController()
         navController?.let { binding.mainBottomNav.setupWithNavController(it) }
+//        binding.mainBottomNav.setOnItemSelectedListener {
+//            item ->
+//                if (item.itemId == R.id.searchActivity)
+//                    startActivity(Intent(this, SearchActivity::class.java))
+//                else {
+//                    navController?.let {
+//                        NavigationUI.onNavDestinationSelected(item, it) }
+//                }
+//            return@setOnItemSelectedListener true
+//        }
+//        binding.mainBottomNav.selectedItemId = R.id.homeFragment
     }
 }
--- app/src/main/java/com/kud/hanzan/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kud/hanzan/ui/home/HomeFragment.kt
@@ -1,5 +1,8 @@
 package com.kud.hanzan.ui.home
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.kud.hanzan.R
 import com.kud.hanzan.databinding.FragmentHomeBinding
 import com.kud.hanzan.utils.BaseFragment
@@ -7,4 +10,16 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListener()
+    }
+    private fun initListener(){
+        with(binding){
+            homeSearchLayout.setOnClickListener {
+                val action = HomeFragmentDirections.actionHomeFragmentToSearchActivity()
+                findNavController().navigate(action)
+            }
+        }
+    }
 }
