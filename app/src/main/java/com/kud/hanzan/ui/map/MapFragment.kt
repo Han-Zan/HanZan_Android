8acfaa19469593dd707537fb4d98d1d192e70a95
nerw173 <nerw173@gmail.com>
2022-09-15 02:12:32 +0900

    [UPD] 지도 핀 추가

--- app/src/main/java/com/kud/hanzan/ui/map/MapFragment.kt
+++ app/src/main/java/com/kud/hanzan/ui/map/MapFragment.kt
@@ -6,6 +6,9 @@ import com.kud.hanzan.R
 import com.kud.hanzan.databinding.FragmentMapBinding
 import com.kud.hanzan.utils.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import net.daum.mf.map.api.CalloutBalloonAdapter
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
 
 @AndroidEntryPoint
@@ -19,5 +22,36 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
         val mapView = MapView(activity)
         val mapViewContainer = binding.kakaoMapView
         mapViewContainer.addView(mapView)
+        mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
+
+        // 마커 추가
+        val marker = MapPOIItem()
+        marker.apply {
+            itemName = "밀짚모자이크"
+            mapPoint = MapPoint.mapPointWithGeoCoord(37.5044517, 126.9505820)
+//            커스텀 이미지로 가능, 비트맵 이미지만, 벡터 X
+//            markerType = MapPOIItem.MarkerType.CustomImage
+//            customImageResourceId = R.drawable.비트맵이미지
+//            selectedMarkerType = MapPOIItem.MarkerType.CustomImage
+//            customSelectedImageResourceId = R.drawable.marker_red
+//            isCustomImageAutoscale = false
+//            setCustomImageAnchor(0.5f, 1.0f)
+            //블루핀, 옐로우핀, 레드핀도 가능
+            markerType = MapPOIItem.MarkerType.RedPin
+        }
+        mapView.addPOIItem(marker)
+    }
+
+    class CustomBalloonAdapter(): CalloutBalloonAdapter{
+        // 마커 클릭시 나오는 말풍선
+        override fun getCalloutBalloon(p0: MapPOIItem?): View {
+            TODO("Not yet implemented")
+        }
+
+        // 말풍선 클릭시 event
+        override fun getPressedCalloutBalloon(p0: MapPOIItem?): View {
+            TODO("Not yet implemented")
+        }
+
     }
 }
