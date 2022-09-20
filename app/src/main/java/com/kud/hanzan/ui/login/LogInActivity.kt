a44d6d90edb9206ebe977db4abdb5f073a9fa343
Tae Woo <t.wooo@kakao.com>
2022-09-20 23:20:02 +0900

    login activity develop

--- app/src/main/java/com/kud/hanzan/ui/login/LogInActivity.kt
+++ app/src/main/java/com/kud/hanzan/ui/login/LogInActivity.kt
@@ -1,14 +1,18 @@
 package com.kud.hanzan.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import com.kud.hanzan.R
 import com.kud.hanzan.databinding.ActivityLogInBinding
+import com.kud.hanzan.ui.MainActivity
 import com.kud.hanzan.utils.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LogInActivity : BaseActivity<ActivityLogInBinding>(R.layout.activity_log_in) {
     override fun initView() {
-
+        binding.loginNextBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
