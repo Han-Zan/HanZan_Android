eb183d318f010b1d2e19c7a3bb7daf9afccb36a1
nerw173 <nerw173@gmail.com>
2022-09-22 16:55:52 +0900

    [UPD] 현재 위치 이동 추가

--- app/src/main/java/com/kud/hanzan/ui/map/MapFragment.kt
+++ app/src/main/java/com/kud/hanzan/ui/map/MapFragment.kt
@@ -3,17 +3,16 @@ package com.kud.hanzan.ui.map
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
+import android.os.Looper
 import android.view.View
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.SearchView
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.gms.location.*
 import com.kud.hanzan.R
 import com.kud.hanzan.databinding.FragmentMapBinding
 import com.kud.hanzan.utils.base.BaseFragment
@@ -25,11 +24,13 @@ import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
 
+
 @AndroidEntryPoint
-class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
+class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), MapView.MapViewEventListener {
+    private lateinit var fusedLocationProviderClient : FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
     private lateinit var mapView: MapView
-    private var isPickUpShown: Boolean = true
-    private var isNearShown: Boolean = true
+
 
     companion object{
         private const val TAG = "MapFragment"
@@ -44,22 +45,26 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initMap()
+        initLocationClient()
         initListener()
         observe()
     }
 
     override fun onResume() {
         super.onResume()
-        isPickUpShown = binding.mapPickupAvailCb.isChecked
-        isNearShown = binding.mapPickupNearCb.isChecked
-        binding.mapPickupAvailCb.text = if (isPickUpShown) "픽업 가능" else "전체"
-        binding.mapPickupNearCb.text = if (isNearShown) "가까운 순" else "정확도 순"
+        binding.isPickupShown = binding.mapPickupNearCb.isChecked
+        binding.isNearShown = binding.mapPickupNearCb.isChecked
     }
+
     // Todo : 현재 위치 찾으려 할 때 권한 항상 check
     private fun initMap(){
         mapView = MapView(activity)
         binding.kakaoMapView.addView(mapView)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
         checkPermission()
+        mapView.setMapViewEventListener {
+
+        }
         // 마커 추가
 //        val marker = MapPOIItem()
 //        marker.apply {
@@ -78,57 +83,20 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
 //        mapView.addPOIItem(marker)
     }
 
-    private fun checkPermission(){
-        val rejectedPermissions = ArrayList<String>()
-        for (permission in requestPermissions){
-            if (ActivityCompat.checkSelfPermission(requireActivity(), permission) != PackageManager.PERMISSION_GRANTED){
-                // 권한 없다면 거절된 리스트에 추가
-                rejectedPermissions.add(permission)
-            }
-        }
-        // 거절된 게 있다면 다시 물어보기
-        if (rejectedPermissions.isNotEmpty()){
-            val array = arrayOfNulls<String>(rejectedPermissions.size)
-            ActivityCompat.requestPermissions(requireActivity(), rejectedPermissions.toArray(array), multiplePermissionCode)
-        } else{
-            mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
-        }
-//        locationPermissionRequest.launch(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
-//            Manifest.permission.ACCESS_COARSE_LOCATION))
-    }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        when(requestCode){
-            multiplePermissionCode -> {
-                if ((grantResults.isNotEmpty()) && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
-                } else{
-                    Toast.makeText(requireContext(), "권한을 허용해주세요.", Toast.LENGTH_SHORT).show()
-                }
-                return
-            }
-            else -> {
-                Toast.makeText(requireContext(), "권한을 허용해주세요.", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
 
     // Todo : 삭제 버튼 눌렀을 때 핀 제거 추가해보기
     private fun initListener(){
         with(binding){
             mapCurrentPosIv.setOnClickListener {
-                checkPermission() }
+                setCurrentLocation() }
             // 체크박스 리스너
-            mapPickupAvailCb.setOnClickListener { isPickUpShown = mapPickupAvailCb.isChecked
-                mapPickupAvailCb.text = if (isPickUpShown) "픽업 가능" else "전체"
+            mapPickupAvailCb.setOnClickListener {
+                isPickupShown = !isPickupShown!!
             }
 
-            mapPickupNearCb.setOnClickListener { isNearShown = mapPickupNearCb.isChecked
-                mapPickupNearCb.text = if (isNearShown) "가까운 순" else "정확도 순"
+            mapPickupNearCb.setOnClickListener {
+                isNearShown = !isNearShown!!
             }
 
             // 서치뷰 리스너
@@ -159,6 +127,126 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
         }
     }
 
+    private fun initLocationClient(){
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        val locationRequest = LocationRequest.create()?.apply {
+            interval = 1000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest!!)
+        val client = LocationServices.getSettingsClient(requireContext())
+        client.checkLocationSettings(builder.build())
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations) {
+                    break
+                }
+            }
+        }
+
+        startLocationUpdates()
+    }
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Toast.makeText(requireContext(), "권한을 허용해주세요", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val locationRequest = LocationRequest.create()?.apply {
+            interval = 1000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    }
+
+    private fun setCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+            location?.let {
+                mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(location.latitude, location.longitude), false)
+            }
+        }
+    }
+
+    private fun checkPermission(){
+        val rejectedPermissions = ArrayList<String>()
+        for (permission in requestPermissions){
+            if (ActivityCompat.checkSelfPermission(requireActivity(), permission) != PackageManager.PERMISSION_GRANTED){
+                // 권한 없다면 거절된 리스트에 추가
+                rejectedPermissions.add(permission)
+            }
+        }
+        // 거절된 게 있다면 다시 물어보기
+        if (rejectedPermissions.isNotEmpty()){
+            val array = arrayOfNulls<String>(rejectedPermissions.size)
+            ActivityCompat.requestPermissions(requireActivity(), rejectedPermissions.toArray(array), multiplePermissionCode)
+        } else{
+            mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeadingWithoutMapMoving
+            setCurrentLocation()
+        }
+//        locationPermissionRequest.launch(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+//            Manifest.permission.ACCESS_COARSE_LOCATION))
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            multiplePermissionCode -> {
+                if ((grantResults.isNotEmpty()) && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeadingWithoutMapMoving
+                    if (ActivityCompat.checkSelfPermission(
+                            requireContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                            requireContext(),
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        return
+                    }
+                    fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+                        location?.let {
+                            mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(location.latitude, location.longitude), false)
+                        }
+                    }
+                } else{
+                    Toast.makeText(requireContext(), "권한을 허용해주세요.", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+            else -> {
+                Toast.makeText(requireContext(), "권한을 허용해주세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     private fun addMarker(name: String,x: String, y: String){
         val marker = MapPOIItem()
         marker.apply {
@@ -177,6 +265,11 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
         mapView.addPOIItem(marker)
     }
 
+    override fun onStop() {
+        super.onStop()
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+
     class CustomBalloonAdapter(): CalloutBalloonAdapter{
         // 마커 클릭시 나오는 말풍선
         override fun getCalloutBalloon(p0: MapPOIItem?): View {
@@ -187,6 +280,43 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
         override fun getPressedCalloutBalloon(p0: MapPOIItem?): View {
             TODO("Not yet implemented")
         }
+
+    }
+    override fun onMapViewCenterPointMoved(p0: MapView?, p1: MapPoint?) {
+        p1?.let { p1.mapPointScreenLocation. }
+    }
+
+    override fun onMapViewInitialized(p0: MapView?) {
+
+    }
+
+
+
+    override fun onMapViewZoomLevelChanged(p0: MapView?, p1: Int) {
+
+    }
+
+    override fun onMapViewSingleTapped(p0: MapView?, p1: MapPoint?) {
+
+    }
+
+    override fun onMapViewDoubleTapped(p0: MapView?, p1: MapPoint?) {
+
+    }
+
+    override fun onMapViewLongPressed(p0: MapView?, p1: MapPoint?) {
+
+    }
+
+    override fun onMapViewDragStarted(p0: MapView?, p1: MapPoint?) {
+
+    }
+
+    override fun onMapViewDragEnded(p0: MapView?, p1: MapPoint?) {
+
+    }
+
+    override fun onMapViewMoveFinished(p0: MapView?, p1: MapPoint?) {
 
     }
 }
