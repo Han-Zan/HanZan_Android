b9b555d0625bf3f7729e666b6954840aa352e373
Tae Woo <t.wooo@kakao.com>
2022-09-22 11:51:30 +0900

    temporary commit

--- app/src/main/java/com/kud/hanzan/ui/login/LoginActivity.kt
+++ app/src/main/java/com/kud/hanzan/ui/login/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.kud.hanzan.ui.login
 
+import android.app.PendingIntent.OnFinished
 import android.content.Intent
+import android.view.View
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.widget.addTextChangedListener
 import com.kud.hanzan.R
@@ -13,61 +15,57 @@ import dagger.hilt.android.AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
     override fun initView() {
 
+        //TODO("Glide 라이브러리를 사용하여 프로필 사진 처리하기")
+
         fun isCorrectUserName(userName: String): Boolean {
-            return userName.length == 13
             //TODO("닉네임 조건 결정하고 구현하기")
+            return userName.length == 13
         }
         binding.loginUserNameET.addTextChangedListener {
             val status = binding.loginUserNameStatusTV
             if (isCorrectUserName(binding.loginUserNameET.text.toString())) {
                 status.text = getString(R.string.user_name_correct)
-                status.setTextColor(resources.getColor(R.color.text_positive))
+                status.setTextColor(resources.getColor(R.color.text_positive, null))
+                binding.isNameChecked = true
             } else {
                 status.text = getString(R.string.user_name_error)
-                status.setTextColor(resources.getColor(R.color.text_negative))
+                status.setTextColor(resources.getColor(R.color.text_negative, null))
+                binding.isNameChecked = false
             }
         }
 
         fun turnOffAllButtons(exceptAge: Int) {
-            if (exceptAge != 20)
-                binding.loginUserAge20.background = ResourcesCompat.getDrawable(resources, R.drawable.bg_white_glass_button_small, null)
-            if (exceptAge != 30)
-                binding.loginUserAge30.background = ResourcesCompat.getDrawable(resources, R.drawable.bg_white_glass_button_small, null)
-            if (exceptAge != 40)
-                binding.loginUserAge40.background = ResourcesCompat.getDrawable(resources, R.drawable.bg_white_glass_button_small, null)
-            if (exceptAge != 50)
-                binding.loginUserAge50.background = ResourcesCompat.getDrawable(resources, R.drawable.bg_white_glass_button_small, null)
-            if (exceptAge != 60)
-                binding.loginUserAge60over.background = ResourcesCompat.getDrawable(resources, R.drawable.bg_white_glass_button_medium, null)
+            binding.isAgeChecked = true
+            binding.loginUserAge20.isChecked = exceptAge == 20
+            binding.loginUserAge30.isChecked = exceptAge == 30
+            binding.loginUserAge40.isChecked = exceptAge == 40
+            binding.loginUserAge50.isChecked = exceptAge == 50
+            binding.loginUserAge60over.isChecked = exceptAge == 60
         }
-        var userAge: Int = 0
+        var userAge = 0
         binding.loginUserAge20.setOnClickListener {
             turnOffAllButtons(20)
-            it.background = ResourcesCompat.getDrawable(resources, R.drawable.bg_yellow_glass_button_small, null)
             userAge = 20
         }
         binding.loginUserAge30.setOnClickListener {
             turnOffAllButtons(30)
-            it.background = ResourcesCompat.getDrawable(resources, R.drawable.bg_yellow_glass_button_small, null)
             userAge = 30
         }
         binding.loginUserAge40.setOnClickListener {
             turnOffAllButtons(40)
-            it.background = ResourcesCompat.getDrawable(resources, R.drawable.bg_yellow_glass_button_small, null)
             userAge = 40
         }
         binding.loginUserAge50.setOnClickListener {
             turnOffAllButtons(50)
-            it.background = ResourcesCompat.getDrawable(resources, R.drawable.bg_yellow_glass_button_small, null)
             userAge = 50
         }
         binding.loginUserAge60over.setOnClickListener {
             turnOffAllButtons(60)
-            it.background = ResourcesCompat.getDrawable(resources, R.drawable.bg_yellow_glass_button_medium, null)
             userAge = 60
         }
 
         binding.loginNextBtn.setOnClickListener {
+            // TODO("DB에 닉네임, 나이정보 POST 하기")
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
