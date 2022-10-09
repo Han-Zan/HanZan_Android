bd5534e9bc4e334c302d3a20873196c56bbb184a
active_dg <nerw173@gmail.com>
2022-10-09 22:15:48 +0900

    [ADD] OCR 추가

--- app/src/main/java/com/kud/hanzan/ui/camera/CameraActivity.kt
+++ app/src/main/java/com/kud/hanzan/ui/camera/CameraActivity.kt
@@ -2,58 +2,63 @@ package com.kud.hanzan.ui.camera
 
 import android.content.pm.PackageManager
 import android.util.Log
+import android.view.ScaleGestureDetector
 import android.widget.Toast
+import androidx.camera.core.Camera
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.TextRecognizer
+import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import com.kud.hanzan.R
 import com.kud.hanzan.databinding.ActivityCameraBinding
 import com.kud.hanzan.utils.base.BaseActivity
+import com.kud.hanzan.vision.TextRecognitionProcessor
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CameraActivity : BaseActivity<ActivityCameraBinding>(R.layout.activity_camera) {
-    companion object {
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS = arrayOf(
-            android.Manifest.permission.CAMERA,
-            android.Manifest.permission.READ_EXTERNAL_STORAGE
-        )
+    private lateinit var cameraSelector : CameraSelector
+    private val cameraProviderFuture by lazy{
+        ProcessCameraProvider.getInstance(this)
+    }
+    private var camera: Camera? = null
+
+    private lateinit var preview : Preview
+
+    // 추가 recognizer
+    private val recognizer by lazy {
+        TextRecognition.getClient(KoreanTextRecognizerOptions.Builder().build())
     }
 
     override fun initView() {
-        if (allPermissionsGranted()){
-            startCamera()
-        } else{
-            ActivityCompat.requestPermissions(
-                this,
-                REQUIRED_PERMISSIONS,
-                REQUEST_CODE_PERMISSIONS
-            )
-        }
+        startCamera()
+        initListener()
     }
 
     private fun startCamera(){
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
         cameraProviderFuture.addListener({
             // Camera의 lifecycle 을 lifecycleowner에 bind 하기 위해 사용
             val cameraProvider = cameraProviderFuture.get()
 
-            val preview = Preview.Builder()
+            preview = Preview.Builder()
                 .build()
                 .also {
                     it.setSurfaceProvider(binding.cameraPreview.surfaceProvider)
                 }
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            setUpPinchToZoom()
 
             try {
                 // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
-
                 // Bind use cases to camera
-                cameraProvider.bindToLifecycle(
+                camera = cameraProvider.bindToLifecycle(
                     this, cameraSelector, preview)
 
             } catch(exc: Exception) {
@@ -63,23 +68,54 @@ class CameraActivity : BaseActivity<ActivityCameraBinding>(R.layout.activity_cam
         }, ContextCompat.getMainExecutor(this))
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSIONS){
-            if (allPermissionsGranted()){
-                startCamera()
-            } else {
-                Toast.makeText(this, "카메라 권한을 허용해주세요.", Toast.LENGTH_SHORT).show()
-                finish()
+    private fun initListener(){
+        with(binding){
+            cameraTurnBtn.setOnClickListener {
+                turnCamera(cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA)
+            }
+            cameraCaptureBtn.setOnClickListener {
+                val result = recognizer.process(InputImage.fromBitmap(cameraPreview.bitmap!!, 0))
+                    .addOnSuccessListener { visionText ->
+                        var text = ""
+                        val resultText = visionText.textBlocks
+                        for (element in resultText){
+                            text += element.text
+                        }
+                        Log.e("camera", text)
+                    }
+
             }
         }
     }
 
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    private fun turnCamera(backCamera: Boolean){
+        val cameraProvider = cameraProviderFuture.get()
+        try {
+            cameraProvider.unbindAll()
+            cameraSelector = if (backCamera) CameraSelector.DEFAULT_FRONT_CAMERA else CameraSelector.DEFAULT_BACK_CAMERA
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+        } catch(exc: Exception) {
+            Log.e("Camera", "Use case binding failed", exc)
+        }
     }
+
+    private fun setUpPinchToZoom() {
+        val listener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+            override fun onScale(detector: ScaleGestureDetector): Boolean {
+                val currentZoomRatio: Float = camera?.cameraInfo?.zoomState?.value?.zoomRatio ?: 1F
+                val delta = detector.scaleFactor
+                camera?.cameraControl?.setZoomRatio(currentZoomRatio * delta)
+                return true
+            }
+        }
+        val scaleGestureDetector = ScaleGestureDetector(this, listener)
+        binding.cameraPreview.setOnTouchListener { _, event ->
+            binding.cameraPreview.post {
+                scaleGestureDetector.onTouchEvent(event)
+            }
+            return@setOnTouchListener true
+        }
+    }
+
+
 }
