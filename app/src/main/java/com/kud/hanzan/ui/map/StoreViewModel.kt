7275fa50c06d154c163eb790b8ae2e43dbad4ea8
active_dg <nerw173@gmail.com>
2022-11-04 19:12:54 +0900

    [ADD] 이미지 선택 다이얼로그

---
+++ app/src/main/java/com/kud/hanzan/ui/map/StoreViewModel.kt
@@ -0,0 +1,12 @@
+package com.kud.hanzan.ui.map
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class StoreViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
