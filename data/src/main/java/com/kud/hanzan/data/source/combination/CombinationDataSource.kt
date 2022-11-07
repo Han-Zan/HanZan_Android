b029d2ad93200ef1c5e2cf580ae40c2714d50bbe
active_dg <nerw173@gmail.com>
2022-11-07 21:53:43 +0900

    [ADD] 주류 리스트 검색 추가

--- data/src/main/java/com/kud/hanzan/data/source/combination/CombinationDataSource.kt
+++ data/src/main/java/com/kud/hanzan/data/source/combination/CombinationDataSource.kt
@@ -5,5 +5,5 @@ import com.kud.hanzan.domain.model.Drink
 import kotlinx.coroutines.flow.Flow
 
 interface CombinationDataSource {
-    fun getDrinkList() : Flow<List<Drink>>
+    fun getDrinkList(userIdx: Long) : Flow<List<Drink>>
 }
