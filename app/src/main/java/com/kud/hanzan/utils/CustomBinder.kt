172f0ba72bbe72a43f14d41213094c836a142a8c
이동건 <nerw173@gmail.com>
2022-11-15 22:16:37 +0900

    [ADD] 바텀 Sheet 추가

--- app/src/main/java/com/kud/hanzan/utils/CustomBinder.kt
+++ app/src/main/java/com/kud/hanzan/utils/CustomBinder.kt
@@ -31,7 +31,7 @@ object CustomBinder {
             .placeholder(placeholder)
             .error(placeholder)
             .diskCacheStrategy(DiskCacheStrategy.NONE)
-            .apply(RequestOptions().fitCenter())
+            .apply(RequestOptions().centerCrop())
             .into(imageView)
     }
 }
